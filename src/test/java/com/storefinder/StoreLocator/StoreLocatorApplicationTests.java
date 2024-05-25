c41bb187b793820db569007aa0498865e8629650
sprabesh122 <sprabesh122@gmail.com>
2024-05-26 02:30:01 +0530

    MongoDb connected

---
+++ src/test/java/com/storefinder/StoreLocator/StoreLocatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storefinder.StoreLocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreLocatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
